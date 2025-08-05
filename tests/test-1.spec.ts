6630f5f75d25d286a50f0a642b92446f3d2e6a9e
triipp <walter@scvsoft.com>
2025-08-05 19:03:42 -0300

    first commit

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,33 @@
+import { test, Browser, Page, expect } from '@playwright/test';
+
+(async () => {
+
+  let browser: Browser;
+  let page: Page;
+
+  test.describe('Navegacion en www.freerangetesters.com', () => {
+    const sections= [
+      {nombre: 'Cursos', url: '/cursos', tituloEsperado: 'Cursos'},
+      {nombre: 'Mentorías', url: '/mentoria-1-1-con-pato', tituloEsperado: 'Mentoría personalizada de avance de carrera para testers de software'},
+      {nombre: 'Recursos', url: '/recursos', tituloEsperado: 'Recursos'}
+    ];
+    for (const section of sections){
+
+        test(`Validate redirection to "${section.nombre}"`, async ({ page }) => {
+        await test.step('Given a user navigating to the website', async () => {
+          await page.goto('https://www.freerangetesters.com/');
+        await test.step(`when user clicks on section ${section.nombre}`, async () => {
+          page.locator('#page_header').getByRole('link',{name: section.nombre, exact: true}).click();
+          await page.waitForURL(`**${section.url}`);
+        })
+        await test.step(`and page title is ${section.tituloEsperado}`, async () => {
+          await expect(page).toHaveTitle(section.tituloEsperado);
+        })
+        
+                
+        })      
+      })
+  }
+  })
+})();
+
